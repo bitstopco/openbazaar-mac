c978b60663448ca4b78ee80746155199d32f0750
Dan Vera <dan@yovu.co>
2014-09-19 19:54:02 -0400

    is it installation or update

--- pow.sh
+++ pow.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-mkdir ~/openbazaar
+if [ -d ~/OpenBazaar -a ! -h ~/OpenBazaar ]
+then
+  # We have an installation so lets update it
+  echo "yes"
+else
+  # We cant find a previous installation.
+
+  cd ~/
+  wget https://github.com/OpenBazaar/OpenBazaar/archive/v"$1".zip
+  unzip v"$1".zip
+  mv OpenBazaar-0.1.0/ OpenBazaar
+  rm -rf v"$1".zip
+
+fi
+
