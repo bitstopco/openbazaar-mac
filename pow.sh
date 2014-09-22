b47f6b7373b129d8dfb3b55a19ab1bd0ce7bb162
Dan Vera <dan@yovu.co>
2014-09-22 12:43:23 -0400

    brew

--- pow.sh
+++ pow.sh
@@ -24,6 +24,25 @@ function doneMessage {
   echo ""
 }
 
+
+function brewDoctor {
+  if ! brew doctor; then
+    echo ""
+    echo "'brew doctor' did not exit cleanly! This may be okay. Read above."
+    echo ""
+    read -p "Press [Enter] to continue anyway or [ctrl + c] to exit and do what the doctor says..."
+  fi
+}
+
+function brewUpgrade {
+  if ! brew upgrade; then
+    echo ""
+    echo "There were errors when attempting to 'brew upgrade' and there could be issues with the installation of OpenBazaar."
+    echo ""
+    read -p "Press [Enter] to continue anyway or [ctrl + c] to exit and fix those errors."
+  fi
+}
+
 function installMac {
   #print commands (useful for debugging)
   #set -x  #disabled because the echos and stdout are verbose enough to see progress
